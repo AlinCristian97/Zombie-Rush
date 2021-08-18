29fc257d271dd1c0db7144a0d900ce251d8f9c6e
Alin Apostol <alin.cristian977@gmail.com>
2021-08-18 04:35:02 +0300

    Added Enemy AI that chases the Player

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+    private NavMeshAgent _navMeshAgent;
+
+    private void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        _navMeshAgent.SetDestination(_target.position);
+    }
+}
