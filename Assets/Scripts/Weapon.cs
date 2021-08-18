6c117784fd30e7f0a490eafe26409113753e5809
Alin Apostol <alin.cristian977@gmail.com>
2021-08-18 21:06:12 +0300

    Added Enemy Health and Weapon Damage

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -6,6 +6,7 @@ public class Weapon : MonoBehaviour
 {
     [SerializeField] Camera _fPCamera;
     [SerializeField] float _range = 100f;
+    [SerializeField] float _damage = 30f;
 
     void Update()
     {
@@ -18,7 +19,13 @@ public class Weapon : MonoBehaviour
     private void Shoot()
     {
         RaycastHit hit;
-        Physics.Raycast(_fPCamera.transform.position, _fPCamera.transform.forward, out hit, _range);
-        Debug.Log("I hit this thing: " + hit.transform.name);
+        if (Physics.Raycast(_fPCamera.transform.position, _fPCamera.transform.forward, out hit, _range))
+        {
+            Debug.Log("I hit this thing: " + hit.transform.name);
+            // TODO: add some hit effect for visual players
+            var target = hit.transform.GetComponent<EnemyHealth>();
+            if (target == null) return;
+            target.TakeDamage(_damage);
+        }
     }
 }
