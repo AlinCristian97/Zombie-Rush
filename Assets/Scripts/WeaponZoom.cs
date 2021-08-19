4571d54eb29161a9377bf4b60bdb99544e81e53c
Alin Apostol <alin.cristian977@gmail.com>
2021-08-19 14:39:19 +0300

    Adjusted Mouse Sensitivity based on the Weapon Zoom level

--- Assets/Scripts/WeaponZoom.cs
+++ Assets/Scripts/WeaponZoom.cs
@@ -1,14 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class WeaponZoom : MonoBehaviour
 {
     [SerializeField] private Camera _fpsCamera;
     [SerializeField] private float _zoomedOutFOV = 60f;
     [SerializeField] private float _zoomedInFOV = 20f;
+    [SerializeField] private float _zoomOutSensitivity = 2f;
+    [SerializeField] private float _zoomInSensitivity = .5f;
+
+    private RigidbodyFirstPersonController _fpsController;
 
     private bool _zoomedInToggle;
+    
+    private void Start()
+    {
+        _fpsController = GetComponent<RigidbodyFirstPersonController>();
+    }
 
     private void Update()
     {
@@ -18,11 +28,15 @@ public class WeaponZoom : MonoBehaviour
             {
                 _zoomedInToggle = true;
                 _fpsCamera.fieldOfView = _zoomedInFOV;
+                _fpsController.mouseLook.XSensitivity = _zoomInSensitivity;
+                _fpsController.mouseLook.YSensitivity = _zoomInSensitivity;
             }
             else
             {
                 _zoomedInToggle = false;
                 _fpsCamera.fieldOfView = _zoomedOutFOV;
+                _fpsController.mouseLook.XSensitivity = _zoomOutSensitivity;
+                _fpsController.mouseLook.YSensitivity = _zoomOutSensitivity;
             }
         }
     }
