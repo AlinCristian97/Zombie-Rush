a56b0b459f228dcebf36197b70860aa2fafa31f7
Alin Apostol <alin.cristian977@gmail.com>
2021-08-19 16:14:26 +0300

    Weapons Switch based on Player Input

--- Assets/Scripts/WeaponSwitcher.cs
+++ Assets/Scripts/WeaponSwitcher.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,6 +10,61 @@ public class WeaponSwitcher : MonoBehaviour
     private void Start()
     {
         SetWeaponActive();
+    }
+
+    private void Update()
+    {
+        int previousWeapon = _currentWeapon;
+
+        ProcessKeyInput();
+        ProcessScrollWheelInput();
+
+        if (previousWeapon != _currentWeapon)
+        {
+            SetWeaponActive();
+        }
+    }
+
+    private void ProcessScrollWheelInput()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            if (_currentWeapon >= transform.childCount - 1)
+            {
+                _currentWeapon = 0;
+            }
+            else
+            {
+                _currentWeapon++;
+            }
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            if (_currentWeapon <= 0)
+            {
+                _currentWeapon = transform.childCount - 1;
+            }
+            else
+            {
+                _currentWeapon--;
+            }
+        }
+    }
+
+    private void ProcessKeyInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            _currentWeapon = 0;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            _currentWeapon = 1;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            _currentWeapon = 2;
+        }
     }
 
     private void SetWeaponActive()
