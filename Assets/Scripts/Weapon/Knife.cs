eccad7293ece89b34c2443d50122af01bb67481c
Alin Apostol <alin.cristian977@gmail.com>
2021-10-02 06:48:28 +0300

    Scripted Knife attack logic

--- Assets/Scripts/Weapon/Knife.cs
+++ Assets/Scripts/Weapon/Knife.cs
@@ -1,14 +1,50 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
+using Enemy;
 using UnityEngine;
 
 namespace Weapon
 {
+    [RequireComponent(typeof(Collider))]
+    [RequireComponent(typeof(Animator))]
     public class Knife : Weapon
     {
+        private Animator _animator;
+        private Collider _collider;
+
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+            _collider = GetComponent<Collider>();
+            
+            DeactivateCollider();
+        }
+
         protected override void Attack()
         {
-            Debug.Log("Knife attack!");
-            //knife attack
+            _animator.SetTrigger("Attack");
+        }
+
+        #region Animator Trigger Functions
+
+        private void ActivateCollider()
+        {
+            _collider.enabled = true;
+        }
+
+        private void DeactivateCollider()
+        {
+            _collider.enabled = false;
+        }
+
+        #endregion
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Enemy"))
+            {
+                other.gameObject.GetComponent<Health>().TakeDamage(Damage);
+            }
         }
     }
 }
