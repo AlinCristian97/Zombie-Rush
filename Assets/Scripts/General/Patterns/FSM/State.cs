afa45416a679c2a3cd67337639e3b782aa777f52
Alin Apostol <alin.cristian977@gmail.com>
2021-10-03 13:48:52 +0300

    Started implementing FSM pattern for Enemy AI

---
+++ Assets/Scripts/General/Patterns/FSM/State.cs
@@ -0,0 +1,9 @@
+﻿namespace General.Patterns.State
+{
+    public abstract class State
+    {
+        public abstract void Enter();
+        public abstract void Exit();
+        public abstract void Execute();
+    }
+}
---
+++ Assets/Scripts/General/Patterns/FSM/StateMachine.cs
@@ -0,0 +1,20 @@
+namespace General.Patterns.State.FSM
+{
+    public class StateMachine
+    {
+        public State CurrentState { get; private set; }
+
+        public void Initialize(State startingState)
+        {
+            CurrentState = startingState;
+            CurrentState.Enter();
+        }
+
+        public void ChangeState(State newState)
+        {
+            CurrentState.Exit();
+            CurrentState = newState;
+            CurrentState.Enter();
+        }
+    }
+}
